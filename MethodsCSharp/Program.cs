46c8b2090cc1053044e5d83052bb24ae5b824365
matthew.murray <matthewmurray14@gmail.com>
2023-04-17 10:26:25 -0500

    First coommit for Methods E 1

---
+++ MethodsCSharp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+//changes so hopefully I can push this up
+
+
+namespace MethodsOne
+{
+    class Program
+    { 
+        public static void oldMan()
+        {
+            Console.WriteLine("What?! Huh?!!");
+            int userRate = int.Parse(Console.ReadLine());
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, what is your name?");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine($"Nice to meet you {userName}! What do you like to do on your days off?");
+            string userACT = Console.ReadLine();
+            
+            Console.WriteLine("That Sounds like fun! On a scale of 1 to 10, how good are you?");
+            int userRate = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What!? You'll have to speak up sonny! I'm an old man.");
+
+            oldMan();
+            oldMan();
+            oldMan();
+            
+            Console.WriteLine("I don't know what you are trying to say to me! You young people today. Back in my day, we showed respect to elders!");
+
+            Console.WriteLine($"Today, {userName}, walked into the store to get supplies for his trip. An older man came up to him asking questions. {userName} did answer his question with, {userRate} but, this man just could not understand it.");
+    }
+
+    }
+
+
+
+
+
+
+}
